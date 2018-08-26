2d98445f7dfb8c4d858bee8c1cf7c738f0acb451
Diego Acosta <diegonake@gmail.com>
2018-08-26 03:21:56 +0200

    fix example and nest animal schema

--- example/index.js
+++ example/index.js
@@ -6,29 +6,22 @@ const HapiMongoose = require('../lib');
 const plugin = {
     plugin: HapiMongoose,
     options: {
-        connections: [
-            {
-                uri: 'mongodb://localhost:27017/myapp'
-            },
-            {
-                alias: 'safebox',
-                uri: 'mongodb://localhost:27017/secrets',
-                schemaPatterns: [
-                    'src/schemas/**/*.js',
-                    '!*.{md,json}'
-                ],
-                options: {
-                    auth: {
-                        user: 'admin',
-                        password: 'pa55w0rd'
-                    },
-                    autoIndex: false,
-                    bufferCommands: true
-                }
+        connection: {
+            uri: 'mongodb://localhost:27017/test',
+            schemaPatterns: [
+                'test/schemas',
+                '!.json'
+            ],
+            options: {
+                autoIndex: false,
+                bufferCommands: true
             }
-        ]
+        }
     }
 };
 
-const server = new Hapi.server();
-server.register(plugin);
+(async () => {
+
+    const server = new Hapi.server({ debug: { log: '*' } });
+    await server.register(plugin);
+})();
