4ea9b75365acb749b3cd3f77a05ce13a483c5655
Diego Acosta <diegonake@gmail.com>
2018-08-26 05:36:34 +0200

    fix schema declaration

--- test/schemas/fns/admin.js
+++ test/schemas/fns/admin.js
@@ -2,17 +2,17 @@
 
 const Mongoose = require('mongoose');
 
-const adminSchema = new Mongoose.Schema({
-    name: String,
-    last: String
-});
-
 module.exports = function (server) {
 
-    adminSchema.post('save', (doc) => {
+    const schema = new Mongoose.Schema({
+        name: String,
+        last: String
+    });
+
+    schema.post('save', (doc) => {
 
         server.events.emit('admin-created', doc.toObject());
     });
 
-    return adminSchema;
+    return schema;
 };
