743b8109f8398da626176e729643ba130eb9aba6
Diego Acosta <diegonake@gmail.com>
2018-12-10 21:17:24 +0100

    remove --no-deprecation flag

--- test/schemas/animal/animal.js
+++ test/schemas/animal/animal.js
@@ -8,10 +8,6 @@ const animalSchema = new Mongoose.Schema({
     tags: { type: [String], index: true }
 });
 
-// FIXME(nakardo): indexes causes deprecation warnings.
-// remove `--no-deprecation` flag from test script once it's fixed on mongoose.
-// See: https://github.com/Automattic/mongoose/issues/6880
-
 animalSchema.index({ name: 1, type: -1 });
 
 module.exports = animalSchema;
